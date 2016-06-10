4cb8e44fc33e6a15ff3cebe3b4a268b335c29de6
fieldkid <phdfieldkidpro@gmail.com>
2016-06-10 15:25:05 +0700

    Add project files.

---
+++ src/dotnetcore_MVC6_example/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcore_MVC6_example.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ src/dotnetcore_MVC6_example/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcore_MVC6_example.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
